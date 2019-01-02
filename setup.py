036840fa33a60a6dcf1c9741917ec6bdaba96560
yy <mengyy_linux@163.com>
2019-01-02 09:06:57 +0800

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hack12306",
-    version="0.0.1",
+    version="0.0.2",
     author="Meng yangyang",
     author_email="mengyy_linux@163.com",
     description="Hack 12306",
